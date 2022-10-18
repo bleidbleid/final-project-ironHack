a3637f93c458102f4ed79f614e1a2bde32a04037
Maria Bladé <mariablade2000+git@gmail.com>
2022-10-18 12:05:24 +0200

    router

---
+++ docs/assets/Home.b4d2b048.js
@@ -0,0 +1,1 @@
+import{o as e,c as o}from"./index.bbe3d747.js";const n={__name:"Home",setup(t){return(c,r)=>(e(),o("h1",null,"Home"))}};export{n as default};
---
+++ docs/assets/Login.ed21b0f4.js
@@ -0,0 +1,1 @@
+import{o as e,c as o}from"./index.bbe3d747.js";const a={__name:"Login",setup(n){return(t,c)=>(e(),o("h1",null,"Login"))}};export{a as default};
